6ffef2fc4984ee22dd559fec13421e0bf4866cb3
mstanka <mice78.mm@gmail.com>
2021-02-18 14:09:30 +0100

    add sorting

--- script.js
+++ script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = movements => {
+const displayMovements = (movements, sort = false) => {
   containerMovements.innerHTML = '';
 
-  movements.forEach((mov, i) => {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -182,7 +184,7 @@ btnLoan.addEventListener('click', e => {
 
     // udpate UI
     updateUI(currentAcc);
- 
+
     // clear input
     inputLoanAmount.value = '';
   }
@@ -207,4 +209,11 @@ btnClose.addEventListener('click', e => {
 
   // clear inputs
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', e => {
+  e.preventDefault();
+  displayMovements(currentAcc.movements, !sorted);
+  sorted = !sorted;
 });
